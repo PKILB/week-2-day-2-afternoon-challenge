166315bdd3393f3a5af38da5b1bf1fb3a866aede
Payton Boone <paytonboone30@gmail.com>
2023-01-24 14:47:58 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let packages = [
+    {
+        to: 'Boone',
+        trackingNumber: '60 lbs',
+        hasPackage: true,
+    },
+    {
+        to: 'Harold',
+        trackingNumber: '75 lbs',
+        hasPackage: true,
+    },
+    {
+        to: 'Nehemiah',
+        trackingNumber: '20 lbs',
+        hasPackage: true,
+    },
+    {
+        to: 'Terry',
+        trackingNumber: '100 lbs',
+        hasPackage: true,
+    }
+  
+]
