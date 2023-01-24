495b40dcf3d11d1a5ac6bb7ca7201d0462f091ab
Payton Boone <paytonboone30@gmail.com>
2023-01-24 14:53:07 -0700

    array finished

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 let packages = [
     {
+        priorityLevel: 'same-day',
         to: 'Boone',
         trackingNumber: '60 lbs',
+        isFragile: false,
         hasPackage: true,
     },
     {
+        priorityLevel: 'free',
         to: 'Harold',
         trackingNumber: '75 lbs',
+        isFragile: true,
         hasPackage: true,
     },
     {
+        priorityLevel: 'free',
         to: 'Nehemiah',
         trackingNumber: '20 lbs',
+        isFragile: false,
         hasPackage: true,
     },
     {
+        priorityLevel: 'free',
         to: 'Terry',
         trackingNumber: '100 lbs',
+        isFragile: true,
         hasPackage: true,
     }
   
